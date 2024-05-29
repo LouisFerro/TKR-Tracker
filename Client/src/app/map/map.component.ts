5e2b258eea4ce00d96e728637153225241419aeb
Louis Ferro <FER22362@spengergasse.at>
2024-05-29 12:16:24 +0200

    Initialization. Implementation of map.

---
+++ Client/src/app/map/map.component.ts
@@ -0,0 +1,27 @@
+import { AfterViewInit, Component } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  standalone: true,
+  imports: [],
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.css'
+})
+
+export class MapComponent implements AfterViewInit {
+  private map: L.Map | undefined;
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    this.map = L.map('map').setView([48.2081, 16.3713], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(this.map);
+  }
+}
